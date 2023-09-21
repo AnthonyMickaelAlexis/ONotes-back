0633eaa41aa1a27bf7248f6864e6111e953e300f
hector colard <h1.colard@gmail.com>
2023-09-21 16:00:21 +0200

    feat/007authentication-sanctum[WIP]: Mise en place des controllers et des routes d'authent/login

--- o-notes/database/seeders/DatabaseSeeder.php
+++ o-notes/database/seeders/DatabaseSeeder.php
@@ -19,7 +19,6 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this->call([
-            UserSeeder::class,
             CategorySeeder::class,
             SubCategorySeeder::class,
             ArticleSeeder::class,
