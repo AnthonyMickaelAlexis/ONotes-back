c795059674901ea37f42b13bdd2386aa17368ffb
Gabriel Boubée <gabriel@dayries.com>
2023-10-09 14:02:55 +0200

    feat/009 : clean code v2

--- o-notes/app/Http/Controllers/ArticleController.php
+++ o-notes/app/Http/Controllers/ArticleController.php
@@ -57,7 +57,7 @@ class ArticleController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show($id)
+    public function show(string $id)
     {
         $article = Article::find($id);
         if (!empty($article)) {
