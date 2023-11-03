57debd670001b29280f97a2cb768ebf571fa0191
Gabriel Boubée <gabrielboubeepro@gmail.com>
2023-11-03 11:21:39 +0100

    fix condition image

--- o-notes/app/Http/Controllers/ArticleController.php
+++ o-notes/app/Http/Controllers/ArticleController.php
@@ -83,7 +83,7 @@ class ArticleController extends Controller
                     'text_content' => $request->text_content,
                     'file_content' => $request->file_content,
                     'resume' => $request->resume,
-                    'banner' => '/img/' . $imageName ?? null,
+                    'banner' => isset($imageName) ? '/img/' . $imageName : null,
                     'user_id' => $request->user()->id,
                     'subcategory_id' => $request->subcategory_id,
                     'status' => $request->status,
